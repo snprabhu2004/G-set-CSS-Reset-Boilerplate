790383a5d5422217627f51ac8f8eac08ea3cfb6a
gavmck <gavyn.mckenzie@holidayextras.com>
2012-03-23 16:43:39 +0000

    Updates from past few weeks
    Updated structure, unsemantic helper classes now mixins instead

---
+++ js/script.js
@@ -0,0 +1,32 @@
+/* Author:
+	Gavyn McKenzie
+	Initialising plugins from plugins.js
+*/
+
+
+$(document).ready(function(){
+	
+	// Run Matt Kersley's jQuery Responsive menu plugin (see plugins.js)
+	if ($.fn.mobileMenu) {
+		$('ol#id').mobileMenu({
+			switchWidth: 768,                   // width (in px to switch at)
+			topOptionText: 'Choose a page',     // first option text
+			indentString: '&nbsp;&nbsp;&nbsp;'  // string for indenting nested items
+		});
+	}
+
+	// Run Mathias Bynens jQuery placeholder plugin (see plugins.js)
+	if ($.fn.placeholder) {
+		$('input, textarea').placeholder();		
+	}
+	
+	//Scale fix on iPhone/iPad when switching between portrait and landscape
+	MBP.scaleFix();
+});
+
+
+
+
+
+
+
